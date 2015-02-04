2dc1669f9e07b449e0317644165fac7edc0c99af
Sebastian Daschner <git@sebastian-daschner.de>
2015-02-04 10:04:22 +0100

    updated license

--- src/main/java/de/sebastian_daschner/todos/business/RESTConfiguration.java
+++ src/main/java/de/sebastian_daschner/todos/business/RESTConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Sebastian Daschner, sebastian-daschner.de
+ * Copyright (C) 2015 Sebastian Daschner, sebastian-daschner.de
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
